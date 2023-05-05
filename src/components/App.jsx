4f137fadaa2888e64274ef9c11f31ca4c46546d9
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-05-05 14:23:25 -0300

    doesnt work yet need testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import {Clock} from "./main/Clock.jsx"
+import {useState} from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import {ChangeTime1, ChangeTime2} from "../store/slices/timers"
 
 export const App = () => {
     const {timer1,timer2} = useSelector((state)=>state.timerReducer)
+
+    const [currentTimer, setCurrentTimer] = useState("")
+
+    let firstTimer=useEffect(() => {
+        const interval = setInterval(() => {
+          //here goes the function
+        }, 1000);
+      
+        return () => clearInterval(interval);
+      }, []);
+    let secondTimer=useEffect(() => {
+        const interval = setInterval(() => {
+          //here goes the function
+        }, 1000);
     
+        return () => clearInterval(interval);
+      }, []);
   return (
     <>
     <main>
